496149f46cef58199a63753b01d5324b286c23d4
Jiří Tomášek <jirka.tomasek@email.cz>
2026-09-07 17:14:11 +0200

    increased velocity command

--- src/tasks/velocity/config/pawo_10dof/env_cfgs.py
+++ src/tasks/velocity/config/pawo_10dof/env_cfgs.py
@@ -135,8 +135,8 @@ def pawo_flat_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
   if play:
     twist_cmd = cfg.commands["twist"]
     assert isinstance(twist_cmd, UniformVelocityCommandCfg)
-    twist_cmd.ranges.lin_vel_x = (-0.2, .5)
-    twist_cmd.ranges.lin_vel_y = (-0.2, 0.2)
-    twist_cmd.ranges.ang_vel_z = (-0.1, 0.1)
+    twist_cmd.ranges.lin_vel_x = (-0.5, 1.0)
+    twist_cmd.ranges.lin_vel_y = (-0.5, 0.5)
+    twist_cmd.ranges.ang_vel_z = (-0.5, 0.5)
 
   return cfg
